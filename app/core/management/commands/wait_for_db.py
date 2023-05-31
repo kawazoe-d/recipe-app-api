089a4a7252dadbd1bec6d1742a4b025e07c0461f
ShowKawazoe <kawazoe.designare@gmail.com>
2023-05-31 21:36:55 +0900

    formatting by black

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_up = False
         while db_up is False:
             try:
@@ -22,12 +22,12 @@ class Command(BaseCommand):
                 # システム チェック フレームワークを使用して、潜在的な問題がないか Django プロジェクト全体を検査します。
                 # 重大な問題は CommandError として発生します。警告は stderr に出力されます。マイナーな通知は stdout に出力されます。
                 # この場合は、settings.pyのDATABASESで指定されている'default'に接続されているかどうかを確認している
-                self.check(databases=['default'])
+                self.check(databases=["default"])
                 # 'default'に接続されていたらTrueに変更してループから抜ける
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 # エラーを拾った場合は、まだ接続できていないことを標準出力する
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write(self.style.SUCCESS("Database available!"))
